74d37828c5decb71dec2e979a4f9d6ac298b75d7
Apen <cerdanyohann@gmail.com>
2012-02-29 22:08:30 +0100

    add compatibility with swiftmailer and old system

--- classes/class.tx_additionalscheduler_utils.php
+++ classes/class.tx_additionalscheduler_utils.php
@@ -17,54 +17,85 @@ class tx_additionalscheduler_utils
 	 * Send a email using t3lib_htmlmail
 	 */
 
-	public function sendEmail($to, $subject, $message, $type = 'plain', $fromEmail = '', $fromName = '', $charset = 'iso-8859-1', $files = array()) {
-		// send mail
-		$mail = t3lib_div::makeInstance('t3lib_htmlmail');
-		$mail->start();
-		$mail->useBase64();
-		$mail->charset = 'iso-8859-1';
-		$mail->subject = $subject;
+	public function sendEmail($to, $subject, $message, $type = 'plain', $fromEmail = '', $fromName = '', $charset = 'utf-8', $files = array()) {
+		$useSwiftMailer = t3lib_div::compat_version('4.5');
+		if ($useSwiftMailer) {
+			// new TYPO3 swiftmailer code
+			$mail = t3lib_div::makeInstance('t3lib_mail_Message');
+			$mail->setTo(array($to));
+			$mail->setSubject($subject);
+			$mail->setCharset($charset);
+			$mail->setFrom(array($fromEmail => $fromName));
+			$mail->setReplyTo(array($fromEmail => $fromName));
 
-		// from
-		$mail->from_email = $fromEmail;
-		$mail->from_name = $fromName;
-
-		// replyTo
-		$mail->replyto_email = $fromEmail;
-		$mail->replyto_name = $fromName;
-
-		// recipients
-		$mail->setRecipient($to);
-
-		// add Plain
-		if ($type == 'plain') {
-			$mail->addPlain($message);
-		}
-
-		// add HTML
-		if ($type == 'html') {
-			$mail->theParts['html']['content'] = $message;
-			$mail->theParts['html']['path'] = '';
-			$mail->extractMediaLinks();
-			$mail->extractHyperLinks();
-			$mail->fetchHTMLMedia();
-			$mail->substMediaNamesInHTML(0); // 0 = relative
-			$mail->substHREFsInHTML();
-			$mail->setHtml($mail->encodeMsg($mail->theParts['html']['content']));
-		}
-
-		// add Files
-		if (!empty($files)) {
-			foreach ($files as $file) {
-				$mail->addAttachment($file);
+			// add Files
+			if (!empty($files)) {
+				foreach ($files as $file) {
+					$mail->attach(Swift_Attachment::fromPath($file));
+				}
 			}
+
+			// add Plain
+			if ($type == 'plain') {
+				$mail->addPart($message, 'text/plain');
+			}
+
+			// add HTML
+			if ($type == 'html') {
+				$mail->setBody($message, 'text/html');
+			}
+
+			// send
+			$mail->send();
+		} else {
+			// send mail
+			$mail = t3lib_div::makeInstance('t3lib_htmlmail');
+			$mail->start();
+			$mail->useBase64();
+			$mail->charset = $charset;
+			$mail->subject = $subject;
+
+			// from
+			$mail->from_email = $fromEmail;
+			$mail->from_name = $fromName;
+
+			// replyTo
+			$mail->replyto_email = $fromEmail;
+			$mail->replyto_name = $fromName;
+
+			// recipients
+			$mail->setRecipient($to);
+
+			// add Plain
+			if ($type == 'plain') {
+				$mail->addPlain($message);
+			}
+
+			// add HTML
+			if ($type == 'html') {
+				$mail->theParts['html']['content'] = $message;
+				$mail->theParts['html']['path'] = '';
+				$mail->extractMediaLinks();
+				$mail->extractHyperLinks();
+				$mail->fetchHTMLMedia();
+				$mail->substMediaNamesInHTML(0); // 0 = relative
+				$mail->substHREFsInHTML();
+				$mail->setHtml($mail->encodeMsg($mail->theParts['html']['content']));
+			}
+
+			// add Files
+			if (!empty($files)) {
+				foreach ($files as $file) {
+					$mail->addAttachment($file);
+				}
+			}
+
+			// send
+			$mail->setHeaders();
+			$mail->setContent();
+
+			return $mail->sendtheMail();
 		}
-
-		// send
-		$mail->setHeaders();
-		$mail->setContent();
-
-		return $mail->sendtheMail();
 	}
 }
 
