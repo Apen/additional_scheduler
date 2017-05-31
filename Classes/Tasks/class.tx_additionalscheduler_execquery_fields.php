4245c64e7d291764537316bb880fa2b115b263a2
Apen <monica072>
2017-05-31 11:23:09 +0200

    [FEATURE] Compatibility with 8 LTS

--- Classes/Tasks/class.tx_additionalscheduler_execquery_fields.php
+++ Classes/Tasks/class.tx_additionalscheduler_execquery_fields.php
@@ -1,30 +1,23 @@
 <?php
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2015 CERDAN Yohann (cerdanyohann@yahoo.fr)
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-class tx_additionalscheduler_execquery_fields extends \Sng\Additionalscheduler\AdditionalFieldProviderInterface {
+ * The TYPO3 project - inspiring people to share!
+ */
 
-    public function getAdditionalFields(array &$taskInfo, $task, \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $parentObject) {
+class tx_additionalscheduler_execquery_fields extends \Sng\Additionalscheduler\AdditionalFieldProviderInterface
+{
+
+    public function getAdditionalFields(array &$taskInfo, $task, \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $parentObject)
+    {
 
         if (empty($taskInfo['additionalscheduler_exec_query'])) {
             if ($parentObject->CMD == 'edit') {
@@ -82,16 +75,18 @@ class tx_additionalscheduler_execquery_fields extends \Sng\Additionalscheduler\A
         return $additionalFields;
     }
 
-    public function validateAdditionalFields(array &$submittedData, \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $parentObject) {
-        $result = TRUE;
+    public function validateAdditionalFields(array &$submittedData, \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $parentObject)
+    {
+        $result = true;
         if (empty($submittedData['additionalscheduler_exec_query'])) {
             $parentObject->addMessage($GLOBALS['LANG']->sL('LLL:EXT:additional_scheduler/Resources/Private/Language/locallang.xml:savedirerror'), \TYPO3\CMS\Core\Messaging\FlashMessage::ERROR);
-            $result = FALSE;
+            $result = false;
         }
         return $result;
     }
 
-    public function saveAdditionalFields(array $submittedData, \TYPO3\CMS\Scheduler\Task\AbstractTask $task) {
+    public function saveAdditionalFields(array $submittedData, \TYPO3\CMS\Scheduler\Task\AbstractTask $task)
+    {
         $task->query = $submittedData['additionalscheduler_exec_query'];
         $task->email = $submittedData['additionalscheduler_exec_email'];
         $task->emailtemplate = $submittedData['additionalscheduler_exec_emailtemplate'];
