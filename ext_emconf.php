3785583631c2164f5561ed34cca9f4c9df8dc44f
Apen <cerdanyohann@gmail.com>
2020-05-05 21:37:31 +0200

    [TASK] Fix some CLRF

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,22 @@
 <?php
 
 $EM_CONF['additional_scheduler'] = [
-    'title'            => 'Useful tasks in scheduler module',
-    'description'      => 'Useful tasks in the scheduler module : full backup, send query result in mail, exec SH script with reports...',
-    'category'         => 'misc',
-    'version'          => '1.4.0',
-    'state'            => 'stable',
-    'uploadfolder'     => false,
-    'createDirs'       => '',
+    'title' => 'Useful tasks in scheduler module',
+    'description' => 'Useful tasks in the scheduler module : full backup, send query result in mail, exec SH script with reports...',
+    'category' => 'misc',
+    'version' => '1.4.0',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
     'clearcacheonload' => true,
-    'author'           => 'CERDAN Yohann',
-    'author_email'     => 'cerdanyohann@yahoo.fr',
-    'author_company'   => '',
-    'constraints'      => [
-        'depends'   => [
+    'author' => 'CERDAN Yohann',
+    'author_email' => 'cerdanyohann@yahoo.fr',
+    'author_company' => '',
+    'constraints' => [
+        'depends' => [
             'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
-        'suggests'  => [],
+        'suggests' => [],
     ],
 ];
